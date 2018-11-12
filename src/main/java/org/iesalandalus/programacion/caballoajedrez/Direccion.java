7b27aa8f007b830e4205219dc200b55bdc14e400
usuario <usuario@QUIQUE>
2018-11-12 19:45:05 +0100

    Enum de Direccion

---
+++ src/main/java/org/iesalandalus/programacion/caballoajedrez/Direccion.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.iesalandalus.programacion.caballoajedrez;
+
+/**
+ *
+ * @author usuario
+ */
+public enum Direccion {
+     ARRIBA_IZQUIERDA, ARRIBA_DERECHA, DERECHA_ARRIBA, DERECHA_ABAJO, ABAJO_DERECHA, ABAJO_IZQUIERDA, IZQUIERDA_ARRIBA, IZQUIERDA_ABAJO;
+    
+}
