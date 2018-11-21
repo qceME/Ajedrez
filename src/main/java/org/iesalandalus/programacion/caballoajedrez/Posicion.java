3c14ce80b9866a83f706ef5bd7a8de149db3b51a
usuario <usuario@QUIQUE>
2018-11-21 17:48:03 +0100

    Añadiendo los getters y los setters(+validaciones) de la clase posicion

--- src/main/java/org/iesalandalus/programacion/caballoajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/caballoajedrez/Posicion.java
@@ -9,8 +9,41 @@ package org.iesalandalus.programacion.caballoajedrez;
  *
  * @author usuario
  */
+
 public class Posicion {
     private int fila;
-    private String columna;
+    private char columna;
     
+    
+//métodos get y set
+    public int getFila() {
+        return fila;
+    }
+
+    public void setFila(int fila) {
+        if ((fila>=1) && (fila<=8))
+          { 
+            this.fila = fila;
+          }
+        else
+          {throw new IllegalArgumentException("El número de fila pasado como parámetro no es válido");            
+          }  
+            
+        
+    }
+
+    public char getColumna() {
+        return columna;
+    }
+
+    public void setColumna(char columna) {
+        if (columna=='a' || columna=='b' || columna=='c' || 
+            columna=='d' || columna=='e' || columna=='f' || columna=='g' || columna=='h')
+          
+            this.columna = columna;
+          
+        else
+          throw new IllegalArgumentException("La columna pasada como parámetro no es válida");            
+           
+    }
 }
