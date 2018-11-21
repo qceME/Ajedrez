b98954124cfe8977421d1119785337e88704892f
usuario <usuario@QUIQUE>
2018-11-21 19:56:21 +0100

    Clase Caballo + GETTERS&SETTERS

--- src/main/java/org/iesalandalus/programacion/caballoajedrez/Caballo.java
+++ src/main/java/org/iesalandalus/programacion/caballoajedrez/Caballo.java
@@ -9,9 +9,33 @@ package org.iesalandalus.programacion.caballoajedrez;
  *
  * @author usuario
  */
-public class Caballo {
+public class Caballo {                    
+                                           //PREGUNTAR DUDA VALIDACIÓN SET/CONSTRUCTORES 
+                                           //ENTRE DISTINTAS CLASES (EXCEPCION NULL SI YA SE VALIDA EN LA OTRA CLASE)
     
     private Color color;
     private Posicion posicion;
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        if (color==null) throw new IllegalArgumentException("ERROR: No es posible asignar un color nulo.");
+
+        this.color = color;
+    }
+
+    public Posicion getPosicion() {
+        return posicion;
+    }
+
+    public void setPosicion(Posicion posicion) {
+        if (posicion==null) throw new IllegalArgumentException("ERROR: No es posible asignar una posición nula.");
+
+        this.posicion = posicion;
+    }
+    
+    
     
 }
